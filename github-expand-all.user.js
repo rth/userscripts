3a85a892ecd80820aa2a62718d597a02a14e37ea
Loïc Estève <loic.esteve@ymail.com>
2018-02-22 17:13:58 +0100

    Load "items not shown" and "large diffs"

--- github-expand-all.user.js
+++ github-expand-all.user.js
@@ -9,16 +9,60 @@
 
 // Register a shortcut to expands all outdated comments. This is useful to
 // search text inside old comments thas have been folded because the code has
-// been modified after the comment.
-// TODO: this would be nice if this could expand:
-// * big diffs
-// * folded conversations (e.g. https://github.com/scikit-learn/scikit-learn/pull/9012#pullrequestreview-42934289
+// been modified after the comment. At the moment the following content is expanded:
+// * outdated diffs
+// * folded conversations
+//   (e.g. https://github.com/scikit-learn/scikit-learn/pull/9012#pullrequestreview-42934289
 //   215 items not shown)
+// * "Large diffs are not rendered by default
+// * e.g. https://github.com/scikit-learn/scikit-learn/pull/9517/files
+
 (function(){
     'use strict';
-
+    // console.log('github-expand-all.user.js loaded');
     var opened = false;
 
+        function open_outdated_diff_comments() {
+        // console.log('Begin open_outdated_diffs');
+        var outdated_diff_elements = document.getElementsByClassName("outdated-comment");
+        // supporting old github PRs (before "Start Review" feature)
+        var legacy_outdated_diff_elements = document.getElementsByClassName("outdated-diff-comment-container");
+        // transform HTMLCollection objects into arrays and concatenate
+        outdated_diff_elements = Array.concat(Array.from(outdated_diff_elements),
+                                              Array.from(legacy_outdated_diff_elements));
+        console.log('outdated_diff_elements.length: ' + outdated_diff_elements.length);
+        for (var i = 0; i < outdated_diff_elements.length; i++) {
+            var element = outdated_diff_elements[i];
+            if (!opened) {
+                element.classList.add('open');
+            }
+            else {
+                element.classList.remove('open');
+            }
+        }
+        opened = !opened;
+        // console.log('Opened: ' + opened);
+    }
+
+    function load_hidden_items() {
+        var buttons = document.getElementsByClassName('ajax-pagination-btn');
+        if (buttons.length != 0){
+            for (var i = 0; i < buttons.length; i++) {
+                buttons[i].click();
+            }
+            setTimeout(load_hidden_items, 100);
+        } else {
+            console.log('Loaded all hidden items');
+        }
+    }
+
+    function load_large_diffs() {
+        var buttons = document.getElementsByClassName('load-diff-button');
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].click();
+        }
+        console.log('Loaded ' + buttons.length + ' large diffs');
+    }
     document.addEventListener('keydown', function(e) {
         // console.log('Pressed: ' + e.keyCode);
         // console.log('shift: ' + e.shiftKey);
@@ -26,25 +70,10 @@
         // console.log('alt: ', e.altKey);
         // console.log('meta: ' + e.metaKey);
         if (e.keyCode == 80 && !e.shiftKey && e.ctrlKey && e.altKey && !e.metaKey) {
-            // console.log('Pressed alt-ctrl-p')
-            var outdated_diff_elements = document.getElementsByClassName("outdated-comment");
-            // supporting old github PRs (before "Start Review" feature)
-            var legacy_outdated_diff_elements = document.getElementsByClassName("outdated-diff-comment-container");
-            // transform HTMLCollection objects into arrays and concatenate
-            outdated_diff_elements = Array.concat(Array.from(outdated_diff_elements),
-                                                  Array.from(legacy_outdated_diff_elements));
-            console.log('outdated_diff_elements.length: ' + outdated_diff_elements.length);
-            for (var i = 0; i < outdated_diff_elements.length; i++) {
-                var element = outdated_diff_elements[i];
-                if (!opened) {
-                   element.classList.add('open');
-               }
-               else {
-                   element.classList.remove('open');
-               }
-            }
-            opened = !opened;
-            // console.log('Opened: ' + opened);
+            // console.log('Pressed alt-ctrl-p');
+            open_outdated_diff_comments();
+            load_hidden_items();
+            load_large_diffs();
         }
     }, false);
 })();
